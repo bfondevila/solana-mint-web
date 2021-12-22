501808c429245eb40c0480e6f3b6b7073459a003
jgonzalezferrer <javier@gonzalezferrer.com>
2021-12-22 22:36:24 +0000

    fix rarity percentages

--- components/NFTSale/index.jsx
+++ components/NFTSale/index.jsx
@@ -23,93 +23,93 @@ const NFTSale = (props) => {
     },
     {
       img: "/images/collection/oro-rojo-azul.png",
-      rarity: 0.0001,
+      rarity: 0.001,
     },
     {
       img: "/images/collection/rojo-plata-negro.png",
-      rarity: 0.0002,
-    },
-    {
-      img: "/images/collection/azul-plata-morado.png",
-      rarity: 0.0002,
-    },
-    {
-      img: "/images/collection/morado-azul-bronce.png",
-      rarity: 0.0005,
-    },
-    {
-      img: "/images/collection/blanco-verde-bronce.png",
-      rarity: 0.0005,
-    },
-    {
-      img: "/images/collection/negro-blanco-bronce.png",
-      rarity: 0.0005,
-    },
-    {
-      img: "/images/collection/rojo-verde-morado.png",
-      rarity: 0.001,
-    },
-    {
-      img: "/images/collection/blanco-rojo-azul.png",
-      rarity: 0.001,
-    },
-    {
-      img: "/images/collection/blanco-rojo-verde.png",
-      rarity: 0.001,
-    },
-    {
-      img: "/images/collection/blanco-verde-rojo.png",
-      rarity: 0.0013,
-    },
-    {
-      img: "/images/collection/negro-morado-azul.png",
-      rarity: 0.0015,
-    },
-    {
-      img: "/images/collection/negro-verde-morado.png",
-      rarity: 0.0015,
-    },
-    {
-      img: "/images/collection/negro-blanco-rojo.png",
       rarity: 0.002,
     },
     {
-      img: "/images/collection/morado-azul-blanco.png",
-      rarity: 0.004,
+      img: "/images/collection/azul-plata-morado.png",
+      rarity: 0.002,
     },
     {
-      img: "/images/collection/blanco-azul-negro.png",
-      rarity: 0.004,
-    },
-    {
-      img: "/images/collection/azul-blanco-negro.png",
-      rarity: 0.0045,
-    },
-    {
-      img: "/images/collection/morado-verde-blanco.png",
+      img: "/images/collection/morado-azul-bronce.png",
       rarity: 0.005,
     },
     {
+      img: "/images/collection/blanco-verde-bronce.png",
+      rarity: 0.005,
+    },
+    {
+      img: "/images/collection/negro-blanco-bronce.png",
+      rarity: 0.005,
+    },
+    {
+      img: "/images/collection/rojo-verde-morado.png",
+      rarity: 0.01,
+    },
+    {
+      img: "/images/collection/blanco-rojo-azul.png",
+      rarity: 0.01,
+    },
+    {
+      img: "/images/collection/blanco-rojo-verde.png",
+      rarity: 0.01,
+    },
+    {
+      img: "/images/collection/blanco-verde-rojo.png",
+      rarity: 0.013,
+    },
+    {
+      img: "/images/collection/negro-morado-azul.png",
+      rarity: 0.015,
+    },
+    {
+      img: "/images/collection/negro-verde-morado.png",
+      rarity: 0.015,
+    },
+    {
+      img: "/images/collection/negro-blanco-rojo.png",
+      rarity: 0.02,
+    },
+    {
+      img: "/images/collection/morado-azul-blanco.png",
+      rarity: 0.04,
+    },
+    {
+      img: "/images/collection/blanco-azul-negro.png",
+      rarity: 0.04,
+    },
+    {
+      img: "/images/collection/azul-blanco-negro.png",
+      rarity: 0.045,
+    },
+    {
+      img: "/images/collection/morado-verde-blanco.png",
+      rarity: 0.05,
+    },
+    {
       img: "/images/collection/blanco-morado-verde.png",
-      rarity: 0.010,
+      rarity: 0.10,
     }, {
       img: "/images/collection/verde-blanco-negro.png",
-      rarity: 0.010,
+      rarity: 0.10,
     }, {
       img: "/images/collection/morado-negro-blanco.png",
-      rarity: 0.012,
+      rarity: 0.12,
     },
     {
       img: "/images/collection/negro-morado-blanco.png",
-      rarity: 0.012,
+      rarity: 0.12,
     },
     {
       img: "/images/collection/morado-blanco-negro.png",
-      rarity: 0.0132,
+      rarity: 0.132,
     },
     {
       img: "/images/collection/blanco-negro-morado.png",
-      rarity: 0.014,
+      rarity: 0.14,
     },
   ];
 
