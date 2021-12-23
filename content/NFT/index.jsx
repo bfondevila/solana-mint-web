dea715268c9128601e3b2b5a5ae7d09037601e27
jgonzalezferrer <javier@gonzalezferrer.com>
2021-12-23 01:39:29 +0000

    skeleton for buy NFT

---
+++ content/NFT/index.jsx
@@ -0,0 +1,16 @@
+import { Col, Container, Row, Button } from "react-bootstrap";
+import { PROJECT_NAME } from "../../constants/common";
+
+
+
+const NFT = () => {
+  return (
+    <Container>
+    <Row className="text-center">
+      <h1>¿Cómo comprar un NFT?</h1>
+    </Row>
+    </Container>
+  );
+};
+
+export default NFT;
