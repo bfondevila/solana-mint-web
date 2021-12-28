43ad2624a08407adcf966a1299676edc2de5177d
Bruno Fondevila Rodriguez <BrunoFondevila@twinsoft.es>
2021-12-28 14:12:49 +0100

    OpenSea button link now configurable

--- components/OpenseaButton/index.jsx
+++ components/OpenseaButton/index.jsx
@@ -1,13 +1,11 @@
 import { Button } from "react-bootstrap";
+import { OPENSEA_RECENT_LINK } from "../../constants/common";
 import style from "./opensea_button.module.scss";
 
 const OpenseaButton = (props) => {
   return (
     <div className={"text-center " + style.btn_container}>
-      <Button
-        href={"https://testnets.opensea.io/" + props.wallet}
-        target="_blank"
-      >
+      <Button href={OPENSEA_RECENT_LINK} target="_blank">
         <img src="/images/OpenSea.png" width={"150px"}></img>
       </Button>
     </div>
