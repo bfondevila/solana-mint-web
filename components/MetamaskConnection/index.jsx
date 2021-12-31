9f8937353ff72d6ec9641a9794336da45e5e66f1
Bruno Fondevila Rodriguez <BrunoFondevila@twinsoft.es>
2021-12-31 18:17:54 +0100

    Removing fix for metamask installation; it was causing a side effect on the NFT page

--- components/MetamaskConnection/index.jsx
+++ components/MetamaskConnection/index.jsx
@@ -79,27 +79,15 @@ const MetamaskConnection = (props) => {
   }, []);
 
   useEffect(() => {
-    onboarding.current.initialized = initialized;
+    if (initialized) {
+      onboarding.current.initialized = initialized;
+    }
   }, [initialized]);
 
   useEffect(async () => {
     const handleChainChanged = (_chainId) => {
       window.location.reload();
     };
-
-    setTimeout(() => {
-      {
-        /*
-        Bug in metamask onboarding after installing the extension,
-        it can leave the connect button unresponsive, since it will
-        continuously poll for a metamask update.
-        Reloading the page fixes this problem.
-        */
-      }
-      if (!onboarding.current.initialized) {
-        window.location.reload();
-      }
-    }, 5000);
 
     if (MetaMaskOnboarding.isMetaMaskInstalled()) {
       setMetamaskInstalled(true);
@@ -144,7 +132,7 @@ const MetamaskConnection = (props) => {
       if (userWallet === "") {
         window.ethereum
           .request({ method: "eth_requestAccounts" })
-          .then((newAccounts) => handleNewAccounts(newAccounts))
+          .then(handleNewAccounts)
           .catch(() => window.location.reload());
       }
     } else {
